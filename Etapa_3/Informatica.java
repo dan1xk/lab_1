d0fd62b759d1bf7243cbc3b218fa2b7903237d7a
Daniel Roberto <sdanirobert@outlook.com>
2024-11-24 00:35:12 -0300

    init

---
+++ Etapa_3/Informatica.java
@@ -0,0 +1,30 @@
+public class Informatica extends Loja {
+    private double seguroEletronicos;
+
+    // Construtor com todos os atributos
+    public Informatica(String nome, int quantidadeFuncionarios, double salarioBaseFuncionario, Endereco endereco, Data dataFundacao, double seguroEletronicos) {
+        super(nome, quantidadeFuncionarios, salarioBaseFuncionario, endereco, dataFundacao);
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    // Construtor sem o salário base
+    public Informatica(String nome, int quantidadeFuncionarios, Endereco endereco, Data dataFundacao, double seguroEletronicos) {
+        super(nome, quantidadeFuncionarios, endereco, dataFundacao);
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    // Métodos de acesso (getters e setters)
+    public double getSeguroEletronicos() {
+        return seguroEletronicos;
+    }
+
+    public void setSeguroEletronicos(double seguroEletronicos) {
+        this.seguroEletronicos = seguroEletronicos;
+    }
+
+    // Sobrescrevendo o método toString
+    @Override
+    public String toString() {
+        return super.toString() + ", Seguro de Eletrônicos: R$ " + seguroEletronicos;
+    }
+}
